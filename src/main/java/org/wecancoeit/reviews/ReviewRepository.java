b7c6c8adbe17137a96175bda65aa8707eb0b16c1
TreyBrowder <tbrowder7@ymail.com>
2021-12-16 23:26:40 -0500

    added in database into repo file

--- src/main/java/org/wecancoeit/reviews/ReviewRepository.java
+++ src/main/java/org/wecancoeit/reviews/ReviewRepository.java
@@ -11,7 +11,15 @@ public class ReviewRepository {
 
     Map<Long, Review> reviewsList = new HashMap<>();
 
-    //uses varargs to add none or as many reviews as you want
+    //constructor for database
+    public ReviewRepository(){
+        Review callOfDuty = new Review(1l, "Call Of Duty","imageUrl", "reviewCategory", "content", "date");
+        Review haloInfinite = new Review(2l, "Halo Infinite","imageUrl", "reviewCategory", "content", "date");
+        Review apexLegends = new Review(3l, "Apex Legends","imageUrl", "reviewCategory", "content", "date");
+        Review fortnite = new Review(4l, "Fortnite","imageUrl", "reviewCategory", "content", "date");
+    }
+
+    //uses varargs to add none or as many reviews as you want (for testing only)
     public ReviewRepository(Review ...reviewsToAdd) {
         for(Review review: reviewsToAdd){
             reviewsList.put(review.getId(), review);
